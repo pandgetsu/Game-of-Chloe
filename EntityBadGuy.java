18b755e584f5635889d244870998eb02c4a90a48
DarknessEthereal <erickang3@yahoo.com>
2017-05-25 11:02:19 -0700

    Add files via upload

--- EntityBadGuy.java
+++ EntityBadGuy.java
@@ -4,95 +4,106 @@ import java.awt.image.BufferedImage;
 public class EntityBadGuy extends EntityLiving
 {
 
-	private final int ANIMATION_SPEED = 300;
+    private final int ANIMATION_SPEED = 300;
 
-	private Animation standing;
+    private Animation standing;
+    private Projectile chloe = new Projectile(myHandler, myX, myY, myWidth, myHeight, 25);
+    public EntityBadGuy(Handler handler, float x, float y) 
+    {
+        super(handler, x, y, Tile.TILE_WIDTH, Tile.TILE_HEIGHT);
+        // TODO Auto-generated constructor stub
 
-	public EntityBadGuy(Handler handler, float x, float y) 
-	{
-		super(handler, x, y, Tile.TILE_WIDTH, Tile.TILE_HEIGHT);
-		// TODO Auto-generated constructor stub
+        standing = new Animation(ANIMATION_SPEED, Assets.badGuy_standing);
+    }
+    
+    public String getEntitySignature()
+    {
+        return "mob";
+    }
 
-		standing = new Animation(ANIMATION_SPEED, Assets.badGuy_standing);
-	}
-	
-	public String getEntitySignature()
-	{
-		return "mob";
-	}
+    public void tick()
+    {
+        standing.tick();
+        checkSurrounding();
+        move();
+        System.out.println(myHandler.getInstance().getEntityHandler().getEntityList().size());
+        shoot();
+    }
+    
+    public void shoot()
+    {
+        
+        myHandler.getInstance().getEntityHandler().addEntity(chloe);
 
-	public void tick()
-	{
-		standing.tick();
-		checkSurrounding();
-		move();
-	}
-	
-	public void checkSurrounding()
-	{
-		double differenceOfX = 0;
-		double differenceOfY = 0;
-		
-		moveX = 0;
-		moveY = 0;
+    }
+    
+    public void checkSurrounding()
+    {
+        double differenceOfX = 0;
+        double differenceOfY = 0;
+        
+        moveX = 0;
+        moveY = 0;
 
-		//int entityListLastIndex = myHandler.getInstance().getEntityHandler().getEntityList().size() - 1;
-
-
-		try
-		{
-			differenceOfX = myHandler.getInstance().getEntityHandler().getMainCharacter().getMyX() - myX;
-			differenceOfY = myHandler.getInstance().getEntityHandler().getMainCharacter().getMyY() - myY;
-		}catch(NullPointerException e)
-		{
-			e.printStackTrace();
-		}
-
-		double distance = Math.sqrt(Math.pow(differenceOfX, 2)+ Math.pow(differenceOfY, 2)) / 32;
-		//double distance2 = Point2D.distance(myHandler.getInstance().getEntityHandler().getEntityList().get(2).getMyX(), myHandler.getInstance().getEntityHandler().getEntityList().get(2).getMyY(), myX, myY) / 32;
+        //int entityListLastIndex = myHandler.getInstance().getEntityHandler().getEntityList().size() - 1;
 
 
-		if(distance <= 5)
-		{
-			if(myX < myHandler.getInstance().getEntityHandler().getMainCharacter().getMyX() - 32)
-			{
-				moveX++;
-			}else if(myX > myHandler.getInstance().getEntityHandler().getMainCharacter().getMyX() + 32)
-			{
-				moveX--;
-			}
+        try
+        {
+            differenceOfX = myHandler.getInstance().getEntityHandler().getMainCharacter().getMyX() - myX;
+            differenceOfY = myHandler.getInstance().getEntityHandler().getMainCharacter().getMyY() - myY;
+        }catch(NullPointerException e)
+        {
+            e.printStackTrace();
+        }
 
-			if(myY < myHandler.getInstance().getEntityHandler().getMainCharacter().getMyY() - 32)
-			{
-				moveY++;
-			}else if(myY > myHandler.getInstance().getEntityHandler().getMainCharacter().getMyY() + 32)
-			{
-				moveY--;
-			}
-		}
-		
-		if(distance <= 1.5)
-		{
-			attack(myHandler.getInstance().getEntityHandler().getMainCharacter());
-		}
-	}
+        double distance = Math.sqrt(Math.pow(differenceOfX, 2)+ Math.pow(differenceOfY, 2)) / 32;
+        //double distance2 = Point2D.distance(myHandler.getInstance().getEntityHandler().getEntityList().get(2).getMyX(), myHandler.getInstance().getEntityHandler().getEntityList().get(2).getMyY(), myX, myY) / 32;
 
-	public void getAction()
-	{
-		getDamaged();
-	}
 
-	public void render(Graphics g)
-	{
-		if(getStat_HP() > 0)
-		{
-			g.drawImage(getCurrentAnimation(), (int) getMyX(), (int) getMyY(), getMyWidth(), getMyHeight(), null);
-		}
-	}
+        if(distance <= 5)
+        {
+            if(myX < myHandler.getInstance().getEntityHandler().getMainCharacter().getMyX() - 32)
+            {
+                moveX++;
+            }else if(myX > myHandler.getInstance().getEntityHandler().getMainCharacter().getMyX() + 32)
+            {
+                moveX--;
+            }
 
-	private BufferedImage getCurrentAnimation()
-	{
-		return standing.getCurrentSprite();
-	}
+            if(myY < myHandler.getInstance().getEntityHandler().getMainCharacter().getMyY() - 32)
+            {
+                moveY++;
+            }else if(myY > myHandler.getInstance().getEntityHandler().getMainCharacter().getMyY() + 32)
+            {
+                moveY--;
+            }
+        }
+        
+        if(distance <= 1.5)
+        {
+            attack(myHandler.getInstance().getEntityHandler().getMainCharacter());
+        }
+    }
+
+    public void getAction()
+    {
+        getDamaged();
+    }
+
+    public void render(Graphics g)
+    {
+        chloe.render(g);
+        if(getStat_HP() > 0)
+        {
+            g.drawImage(getCurrentAnimation(), (int) getMyX(), (int) getMyY(), getMyWidth(), getMyHeight(), null);
+            
+        }
+    }
+
+    private BufferedImage getCurrentAnimation()
+    {
+        return standing.getCurrentSprite();
+    }
 
 }
